14f165cd14e332ab96d10ed70a76a233e3942bb7
Anonymous CCP Employee <anonymous.employee@ccpgames.com>
2019-02-28 16:47:45 +0800

    Hooked up prometheus-cpp and added a test metric
    - Set up prometheus-cpp linkage via vcpkg.
    - Requires zlib1.dll from prometheus-cpp.  Don't see a static option for that yet.
    - Set up a basic counter metric.
    - Navigating to localhost:20800 while the test is running shows the pullable metric page.
    - This currently doesn't shut down correctly.
    - refs #2

--- prometheus_module/module.cpp
+++ prometheus_module/module.cpp
@@ -1,5 +1,14 @@
 // STL
+#include <chrono>
+#include <map>
+#include <memory>
 #include <iostream>
+#include <string>
+#include <thread>
+
+// Prometheus
+#include <prometheus/exposer.h>
+#include <prometheus/registry.h>
 
 // Python
 #include <Python.h>
@@ -14,11 +23,49 @@ static PyObject* hello_world(PyObject* self, PyObject* param) {
 	return PyInt_FromLong(0L);
 }
 
+prometheus::Exposer exposer("127.0.0.1:20800");
+std::shared_ptr<prometheus::Registry> registry = std::make_shared<prometheus::Registry>();
+prometheus::Counter* counter = nullptr;
+
+static PyObject* test_setup(PyObject* self, PyObject* param) {
+	std::cout << "test setup" << std::endl;
+
+	std::map <std::string, std::string> labels = { {"label1", "value1"}, {"label2", "value2"} };
+	auto& counter_family = prometheus::BuildCounter().Name("Test Counter").Labels(labels).Register(*registry);
+	counter = &counter_family.Add(labels);
+
+	exposer.RegisterCollectable(registry);
+
+	return PyInt_FromLong(0L);
+}
+
+static PyObject* test_increment(PyObject* self, PyObject* param) {
+	std::cout << "test increment" << std::endl;
+
+	if (counter == nullptr) {
+		std::cout << "null counter" << std::endl;
+		return PyInt_FromLong(0L);
+	}
+
+	std::cout << "Incrementing once per second for 60 seconds" << std::endl;
+	for (int i = 0; i < 60; i++) {
+		std::cout << i + 1 << " ";
+		counter->Increment();
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+	}
+	std::cout << std::endl;
+
+	return PyInt_FromLong(0L);
+}
+
 
 // Python linkage
 
 static PyMethodDef ModuleMethods[] = {
 	{"hello_world", hello_world, METH_NOARGS, "Hello world"},
+
+	{"test_setup", test_setup, METH_NOARGS, "Test setup"},
+	{"test_increment", test_increment, METH_NOARGS, "Test increment"},
 
     {NULL, NULL, 0, NULL}        /* Sentinel */
 };
--- test/test.py
+++ test/test.py
@@ -1,3 +1,11 @@
 import prometheus_module
 
 prometheus_module.hello_world()
+
+print 'test setup'
+prometheus_module.test_setup()
+
+print 'test increment'
+prometheus_module.test_increment()
+
+raw_input('press enter to quit')
