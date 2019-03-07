38962e4ba28093590dc26503e8b05e0dd0a70ea5
Anonymous CCP Employee <anonymous.employee@ccpgames.com>
2019-03-07 19:26:43 +0800

    Removed TestType
    - It served its purpose.  There are real types to work with now.
    - refs #4

--- prometheus_module/module.cpp
+++ prometheus_module/module.cpp
@@ -17,7 +17,6 @@ static PyObject* PrometheusError;
 
 // Project
 #include "metric_registry.h"
-#include "test_type.h"
 
 
 // Hello world
@@ -27,27 +26,12 @@ static PyObject* hello_world(PyObject* self, PyObject* param) {
 	return PyInt_FromLong(0L);
 }
 
-static PyObject* test_object(PyObject* self, PyObject* param) {
-	TestType test("127.0.0.1:20800");
-
-	std::cout << "Incrementing once per second for 60 seconds" << std::endl;
-	for (int i = 0; i < 60; i++) {
-		std::cout << i + 1 << " ";
-		test.Increment();
-		std::this_thread::sleep_for(std::chrono::seconds(1));
-	}
-	std::cout << std::endl;
-
-	return PyInt_FromLong(0L);
-}
 
 
 // Python linkage
 
 static PyMethodDef ModuleMethods[] = {
 	{"hello_world", hello_world, METH_NOARGS, "Hello world"},
-
-	{"test_object", test_object, METH_NOARGS, "Test object"},
 
     {NULL, NULL, 0, NULL}        /* Sentinel */
 };
@@ -61,6 +45,5 @@ PyMODINIT_FUNC initprometheus_module(void) {
     Py_INCREF(PrometheusError);
     PyModule_AddObject(m, "error", PrometheusError);
 
-	TestType::RegisterPythonObject(m);
 	MetricRegistry::RegisterPythonObject(m);
 }
--- test/test.py
+++ test/test.py
@@ -4,8 +4,7 @@ import prometheus_module
 
 metric_registry = prometheus_module.MetricRegistry()
 
-raw_input('Press enter to begin hosting the /metrics endpoint')
-#test_object = prometheus_module.MetricTest()
+raw_input('Press enter to begin hosting at localhost:20800')
 
 #print 'Incrementing the counter for 10 seconds...'
 #for x in range(10):
@@ -13,10 +12,9 @@ raw_input('Press enter to begin hosting the /metrics endpoint')
 #    test_object.increment()
 #    print x+1
 
-#metric_registry.Serve(":20802")
 metric_registry.Serve()
 
-raw_input('Press enter to terminate the /metrics endpoint')
+raw_input('Press enter to stop hosting')
 metric_registry.StopServing()
 
 raw_input('press enter to quit')
