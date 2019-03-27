0ef35067491aca6b622f1c8448c3de465639f095
Anonymous CCP Employee <anonymous.employee@ccpgames.com>
2019-03-27 21:30:42 +0800

    Removed TestBase.Serve and StopServing
    - Better to use the registry functions directly.  Part of the function of these tests is documentation, and it's easier to read without the unnecessary indirection of these wrapper functions.
    - refs #9

--- test/test.py
+++ test/test.py
@@ -9,25 +9,17 @@ class TestBase(unittest.TestCase):
 
     def setUp(self):
         self.port = '20800'
-        self.url = 'http://localhost:' + self.port
         self.registry = prometheus_module.MetricRegistry()
 
-    def Serve(self, port=''):
+    def Fetch(self, port=''):
         if port == '':
             port = self.port
-        self.registry.Serve(port)
-
-    def StopServing(self):
-        self.registry.StopServing()
-
-    def Fetch(self, url=''):
-        if url == '':
-            url = self.url
+        url = 'http://localhost:' + port
         return urllib2.urlopen(url).read()
 
-    def IsServerListening(self):
+    def IsServerListening(self, port=''):
         try:
-            content = self.Fetch()
+            content = self.Fetch(port)
             has_valid_content = 'exposer' in content
             return has_valid_content
         except urllib2.URLError:
@@ -36,9 +28,9 @@ class TestBase(unittest.TestCase):
 class TestServing(TestBase):
     def test_server_start_stop(self):
         self.assertFalse(self.IsServerListening(), 'Server must not listen until Serve is called')
-        self.Serve()
+        self.registry.Serve(self.port)
         self.assertTrue(self.IsServerListening(), 'Server must listen after Serve is called')
-        self.StopServing()
+        self.registry.StopServing()
         self.assertFalse(self.IsServerListening(), 'Server must stop listening after StopServing is called')
         
     def test_bad_port_formats(self):
@@ -52,6 +44,20 @@ class TestServing(TestBase):
     def test_good_port_formats(self):
         self.assertTrue(True)
         # todo
+        print 'todo'
+
+class TestCounter(TestBase):
+    def setUp(self):
+        TestBase.setUp(self)
+        self.registry.Serve(self.port)
+
+    def tearDown(self):
+        TestBase.tearDown(self)
+        self.registry.StopServing()
+
+    def test_counter_counts(self):
+        self.assertTrue(True)
+
 
 if __name__ == '__main__':
     unittest.main()
