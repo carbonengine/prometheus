d680ae826a919a143b7b4ef5c5fb1c491e648b3e
Anonymous CCP Employee <anonymous.employee@ccpgames.com>
2019-03-29 19:13:08 +0800

    Removed some clutter from demo.py

--- demo/demo.py
+++ demo/demo.py
@@ -8,22 +8,13 @@ raw_input('Press enter to begin hosting at localhost:20800')
 
 metric_registry.Serve("20800")
 
-metric_registry.MakeCounter("Counter with name only")
-counter = metric_registry.MakeCounter("Counter with name and labels", {"label1":"value1","label2":"value2"})
-
-metric_registry.MakeGauge("Gauge with name only")
-gauge = metric_registry.MakeGauge("Gauge with name and labels", {"label1":"value1","label2":"value2"})
-gauge.Set(999.0)
-
-metric_registry.MakeSummary("Summary with name only")
-summary = metric_registry.MakeSummary("Summary with name and labels and quantiles", labels={"label1":"value1","label2":"value2"}, quantiles=[(0.1,0.05),(0.5,0.05),(0.9,0.05)])
-summary.Observe(0)
-
-metric_registry.MakeHistogram("Histogram with name only")
-histogram = metric_registry.MakeHistogram("Histogram with name and labels and buckets", labels={"label1":"value1","label2":"value2"}, boundaries=[1.0,3.0,7.0])
-histogram.Observe(0)
+counter = metric_registry.MakeCounter("demo_counter", {"label1":"value1","label2":"value2"})
+gauge = metric_registry.MakeGauge("demo_gauge", {"label1":"value1","label2":"value2"})
+summary = metric_registry.MakeSummary("demo_summary", labels={"label1":"value1","label2":"value2"}, quantiles=[(0.1,0.05),(0.5,0.05),(0.9,0.05)])
+histogram = metric_registry.MakeHistogram("demo_histogram", labels={"label1":"value1","label2":"value2"}, boundaries=[1.0,3.0,7.0])
 
 print 'Modifying the metrics for 10 seconds...'
+gauge.Set(999.0)
 for x in range(10):
     time.sleep(1)
     counter.Increment()
