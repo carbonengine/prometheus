c31e001ab7b731dc9d0eda3bbe24236bea18b5ae
ccp serpent <162123512+ccp-serpent@users.noreply.github.com>
2025-09-17 14:49:51 +0000

    Fix unit test execution and discovery
    This change also includes a change to test discovery whereby
    we use the default `unittest` loader.

---
+++ tests/python/discover.py
@@ -0,0 +1,21 @@
+import sys
+import unittest
+
+# Mock built modules for test discovery
+sys.modules["prometheus_module"] = sys
+sys.modules["test_native"] = sys
+
+def print_suite(suite):
+    if hasattr(suite, '_exception'):
+        print(suite._exception)
+        sys.exit(1)
+    else:
+        if not hasattr(suite, '__iter__'):
+            print(suite.id())
+        else:
+            [print_suite(suite) for suite in suite]
+
+
+if __name__ == '__main__':
+    suite = unittest.defaultTestLoader.discover('.')
+    print_suite(suite)
