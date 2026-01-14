8a28c84c725bf73839b11537c4ddab79cb8963d4
CCP Toebeans <105929497+ccptoebeans@users.noreply.github.com>
2026-01-14 18:27:38 +0000

    Reset parallel level to 1

--- .teamcity/MacOS/Project.kt
+++ .teamcity/MacOS/Project.kt
@@ -73,7 +73,7 @@ class CarbonBuildMacOS(buildName: String, configType: String, preset: String, ag
         param("env.VCPKG_BINARY_SOURCES", "clear;x-aws,s3://vcpkg-binary-cache-static/cache/,readwrite")
         param("env.X_VCPKG_REGISTRIES_CACHE", "%teamcity.build.checkoutDir%/%github_checkout_folder%/regcache")
         param("env.CMAKE_BUILD_PARALLEL_LEVEL", "8")
-        param("env.CTEST_PARALLEL_LEVEL", "8")
+        param("env.CTEST_PARALLEL_LEVEL", "1")
     }
 
 
